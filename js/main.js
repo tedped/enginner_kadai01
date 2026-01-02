6f1b8fdfec143f4d5a50571168fc41ad65c65e22
tedped <grantee.andiron.8s@icloud.com>
2026-01-02 15:06:10 +0900

    Update HTML, CSS ,and JS

--- js/main.js
+++ js/main.js
@@ -1,19 +1,9 @@
 //エレベーターの初期状態
-const state = {
-  currentFloor: 1,
-  moving: false,
-  door: "closed",
-  direction: "idle", // 'up' | 'down' | 'idle'
-  queue: [],
-};
 
-const elevator = document.querySelector("#elevator");
-
-// ここを変えるだけで階数変更可能
-const FLOOR_COUNT = 6;
-
-// ボタンを入れる親要素
-const buttonsContainer = document.querySelector("#buttons");
+// ここを変えるだけで台数を増減可能
+const ELEVATOR_COUNT = 3;
+// ここを変えるだけで階数を増減可能
+const FLOOR_COUNT = 10;
 // エレベーターの高さ（単位はpx）
 const FLOOR_HEIGHT = 30;
 //エレベーターの移動速度（単位はms）
@@ -22,52 +12,140 @@ const TIME_PER_FLOOR = {
   fast: 150,
 };
 
+// エレベーターを入れる親要素
+const elevatorsContainer = document.querySelector("#elevators");
+const elevators = [];
+// ボタンを入れる親要素
+const buttonsContainer = document.querySelector("#buttons");
+
+elevatorsContainer.textContent = "";
+// 各エレベーターのDOMとstateを生成
+for (let i = 0; i < ELEVATOR_COUNT; i++) {
+  const shaftEl = document.createElement("div");
+  shaftEl.classList.add("shaft");
+
+  const elevatorEl = document.createElement("div");
+  elevatorEl.classList.add("elevator");
+  elevatorEl.style.bottom = "0px";
+
+  const leftDoor = document.createElement("div");
+  leftDoor.className = "door left";
+
+  const rightDoor = document.createElement("div");
+  rightDoor.className = "door right";
+
+  const hi = document.createElement("div");
+  hi.textContent = "🥸";
+  hi.id = "hi";
+
+  elevatorEl.append(leftDoor, rightDoor, hi);
+
+  shaftEl.appendChild(elevatorEl);
+  elevatorsContainer.appendChild(shaftEl);
+
+  //エレベーターの状態を管理するオブジェクトを配列に追加
+  elevators.push({
+    id: i,
+    currentFloor: 1,
+    moving: false,
+    direction: "idle",
+    queue: [],
+    element: elevatorEl,
+    processing: false,
+    door: "closed",
+  });
+}
+
+const shaftHeight = FLOOR_COUNT * FLOOR_HEIGHT;
+document.querySelectorAll(".shaft").forEach((shaft) => {
+  shaft.style.height = `${shaftHeight}px`;
+});
+
+//移動距離の最も小さい、最適なエレベーターを選択する関数
+const selectElevator = (floor) => {
+  let best = elevators[0];
+  let bestDistance = Infinity;
+
+  for (const e of elevators) {
+    const distance = Math.abs(e.currentFloor - floor);
+    if (!e.moving && distance < bestDistance) {
+      best = e;
+      bestDistance = distance;
+    }
+  }
+  return best;
+};
+
 //エレベーターの階数を推移させる関数
-const moveOneFloor = async (nextFloor) => {
-  state.moving = true;
-  closeDoor();
+const moveOneFloor = async (elevatorState, nextFloor) => {
+  elevatorState.moving = true;
+  closeDoor(elevatorState);
 
   //エレベーターの状態遷移が完了してから移動開始するための待機時間
-  await wait(5);
+  await wait(15);
 
-  const distance = Math.abs(state.currentFloor - nextFloor);
+  const distance = Math.abs(elevatorState.currentFloor - nextFloor);
   const duration = distance * TIME_PER_FLOOR.normal;
 
-  elevator.style.transition = ` bottom ${duration}ms linear`;
-  elevator.style.bottom = `${(nextFloor - 1) * FLOOR_HEIGHT}px`;
+  elevatorState.element.style.transition = ` bottom ${duration}ms linear`;
+  elevatorState.element.style.bottom = `${(nextFloor - 1) * FLOOR_HEIGHT}px`;
 
   await wait(duration);
 
-  state.currentFloor = nextFloor;
-  state.moving = false;
+  elevatorState.currentFloor = nextFloor;
+  elevatorState.moving = false;
 };
 
 //エレベーターのドア状態を「閉」から「開」に遷移させるための関数
-const openDoor = () => {
-  elevator.classList.add("door-open");
+const openDoor = async (elevatorState) => {
+  if (elevatorState.door === "open") return;
+
+  elevatorState.door = "open";
+  elevatorState.element.classList.add("door-open");
+
+  await wait(2000);
+
+  closeDoor(elevatorState);
 };
 
 //エレベーターのドア状態を「開」から「閉」に遷移させるための関数
-const closeDoor = () => {
-  elevator.classList.remove("door-open");
+const closeDoor = (elevatorState) => {
+  if (elevatorState.door === "closed") return;
+
+  elevatorState.door = "closed";
+  elevatorState.element.classList.remove("door-open");
 };
 
 //エレベーターの状態遷移に時間がかかることを表現するための関数
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const renderButtons = () => {
-  document.querySelectorAll("button").forEach((btn) => {
-    const floor = Number(btn.dataset.floor);
-    btn.classList.toggle("waiting", state.queue.includes(floor));
-  });
-};
-
 //処理されていないボタン操作をリクエストとしてキューに追加する関数
 //記録
 const addRequest = (floor) => {
-  if (!state.queue.includes(floor)) {
-    state.queue.push(floor);
+  const e = selectElevator(floor); //最適なエレベーターを選択
+
+  // 今いる階を押した場合
+  if (e.currentFloor === floor && !e.moving) {
+    if (e.door === "closed") {
+      openDoor(e);
+    }
+    return;
   }
+
+  if (!e.queue.includes(floor)) {
+    e.queue.push(floor);
+  }
+};
+
+//キューに追加されたリクエストに基づき、ボタンの点灯表示状態を更新する関数
+const renderButtons = () => {
+  document.querySelectorAll("button").forEach((btn) => {
+    const floor = Number(btn.dataset.floor);
+    btn.classList.toggle(
+      "waiting",
+      selectElevator(floor).queue.includes(floor)
+    );
+  });
 };
 
 //エレベーターの移動方向を決定する関数
@@ -78,12 +156,12 @@ const decideDirection = (from, to) => {
 };
 
 //現在の進行方向に基づき、次の階を返す関数
-const getNextFloor = () => {
-  if (state.direction === "up") {
-    return state.currentFloor + 1;
+const getNextFloor = (elevatorState) => {
+  if (elevatorState.direction === "up") {
+    return elevatorState.currentFloor + 1;
   }
-  if (state.direction === "down") {
-    return state.currentFloor - 1;
+  if (elevatorState.direction === "down") {
+    return elevatorState.currentFloor - 1;
   }
   return null;
 };
@@ -94,66 +172,71 @@ let processing = false;
 
 //エレベーターの移動を処理する関数
 //制御
-const processQueue = async () => {
-  if (processing) return;
-  processing = true;
+const processElevatorQueue = async (elevatorState) => {
+  if (elevatorState.processing) return;
+  elevatorState.processing = true;
 
-  while (state.queue.length > 0) {
+  while (elevatorState.queue.length > 0) {
     // direction は最初だけ決める
-    if (state.direction === "idle") {
-      const target = state.queue[0];
-      state.direction = decideDirection(state.currentFloor, target);
+    if (elevatorState.direction === "idle") {
+      const target = elevatorState.queue[0];
+      elevatorState.direction = decideDirection(
+        elevatorState.currentFloor,
+        target
+      );
     }
 
-    const next = getNextFloor();
+    const next = getNextFloor(elevatorState);
     if (next == null) break;
 
-    await moveOneFloor(next);
+    await moveOneFloor(elevatorState, next);
 
     //現在の階がキューに含まれていれば乗降処理を行う
-    if (state.queue.includes(state.currentFloor)) {
+    if (elevatorState.queue.includes(elevatorState.currentFloor)) {
       // 移動前にキューから削除、重複防止
-      state.queue = state.queue.filter((f) => f !== state.currentFloor);
-      renderButtons(); //キューから削除されたボタンの表示を更新
+      elevatorState.queue = elevatorState.queue.filter(
+        (f) => f !== elevatorState.currentFloor
+      );
+      renderButtons(); //キューから削除されたボタンの、点灯表示を更新
 
-      await moveOneFloor(next);
+      await moveOneFloor(elevatorState, next);
       await wait(700);
-      openDoor();
+      openDoor(elevatorState);
       await wait(2000);
-      closeDoor();
+      closeDoor(elevatorState);
       await wait(700);
     }
 
     //進行方向にリクエストがまだあるか確認
     const hasSameDirectionTarget =
-      state.direction === "up"
+      elevatorState.direction === "up"
         ? //some: 配列の中で条件を満たす要素が一つでもあればtrueを返す
-          state.queue.some((f) => f > state.currentFloor)
-        : state.queue.some((f) => f < state.currentFloor);
+          elevatorState.queue.some((f) => f > elevatorState.currentFloor)
+        : elevatorState.queue.some((f) => f < elevatorState.currentFloor);
 
     // 進行方向にリクエストするキューがない場合、状態をidleにする
     if (!hasSameDirectionTarget) {
-      state.direction = "idle";
+      elevatorState.direction = "idle";
     }
   }
 
-  processing = false;
+  elevatorState.processing = false;
 };
-buttonsContainer.innerHTML = "";
+
+buttonsContainer.textContent = "";
 // ボタンを自動生成
 for (let i = FLOOR_COUNT; i >= 1; i--) {
-  // 上から下に並べる場合
+  // 上から下に並べる
   const btn = document.createElement("button");
   btn.textContent = `${i}`;
   btn.dataset.floor = i;
   buttonsContainer.appendChild(btn);
-  shaftHeight = FLOOR_COUNT * FLOOR_HEIGHT;
-  document.querySelector(".shaft").style.height = `${shaftHeight}px`;
 
   //エレベーターのボタンを押したら、押したボタンの階数を数値としてaddRequestに渡し、キューを実行
   btn.addEventListener("click", () => {
-    addRequest(i); //記録
-    renderButtons(); //キューに記録されたボタンの表示を更新
-    processQueue(); //制御を実行
+    const e = selectElevator(i); //最適なエレベーターを選択
+    addRequest(i); //選択したエレベーターをキューに記録
+    renderButtons(); //キューに記録されたボタンの、点灯表示を更新
+    processElevatorQueue(e); //制御を実行
   });
 }
